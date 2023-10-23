5f2858d4438cf65fade6bfc42a349432a3168df5
nguyenhuuday <daynhgcc210205@fpt.edu.vn>
2023-10-23 11:18:52 +0700

    update

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php 
-    $conn = mysqli_connect('localhost','root','','shopa') 
+    $conn = mysqli_connect('ble5mmo2o5v9oouq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','jka4bnmkgj1is9bz','g7vatk9v75qnqehz','dtg3t6quasg6d3sf') 
     or die("Cannot connect database".mysqli_connect_error());
 
     // $conn = mysqli_connect('vkh7buea61avxg07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','mfabx3yyukb47dhz','my4f1ao60ko7khl5','pnhrde2ut64ol8sa') 
